5552c31bf7e189fe82514d5a43ced87cdcca5374
josemg-23 <jose.gomezt19@gmail.com>
2022-03-02 16:38:26 +0000

    1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["A clown", "My cat", "A bear", "Naruto"];
+  let action = ["destroyed", "burned", "ate", "jump on"];
+  let what = ["my car", "my ps5", "my bed", "my pc"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+
+  return who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex];
 };
